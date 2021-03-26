4b95025346412c7a04e07407f7265e823fe19b93
Arvin Fernandez <arvinf07@gmail.com>
2021-03-26 15:59:14 -0400

    established relationship between models

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,7 +1,13 @@
 class LocationsController < ApplicationController
 
+
+  def get_locations
+
+  end
+
   def index
-    
+    locations = Location.all
+    render json: locations.to_json
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :lists
-  resources :locations
+  resources :lists, only: [:index, :show, :create, :update]
+  resources :locations, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
