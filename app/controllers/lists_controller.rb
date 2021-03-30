bc3672b6e6ef5085aee05011eb52ec8e8f3af7fc
Arvin Fernandez <arvinf07@gmail.com>
2021-03-30 09:22:49 -0400

    lists#index returns desired data

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,12 @@
 class ListsController < ApplicationController
+
+  def index
+    lists = List.all
+    render json: lists.to_json(include: {locations: {except: [:created_at, :updated_at]}})
+  end
+
+  def create
+    byebug
+  end
+
 end
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,23 +2,24 @@ require 'rest-client'
 
 class LocationsController < ApplicationController
 
+  # Create new locations based on zip
+  # def get_locations
+  #   url = "https://api.yelp.com/v3/businesses/search?location=10021"
+  #   response = RestClient.get(url, {Authorization: "Bearer API-KEY"})
+  #   JSON.parse(response)['businesses'].each do |biz|
+  #     location = Location.new(name: biz['name'],
+  #     address: biz['location']['display_address'].join(', '),
+  #     phone_num: biz['display_phone'],
+  #     categories: biz['categories'].map{|category| category['title']}.join(', '),
+  #     image_url: biz['image_url'],
+  #     rating: biz['rating'],
+  #     list_id: 1
+  #     )
+  #     location.save
+  #   end
+  #   render json: Location.all.to_json
 
-  def get_locations
-    url = "https://api.yelp.com/v3/businesses/search?location=10026"
-    response = RestClient.get(url, {Authorization: "Bearer })
-    JSON.parse(response)['businesses'].each do |biz|
-      location = Location.new(name: biz['name'],
-      address: biz['location']['display_address'].join(', '),
-      phone_num: biz['display_phone'],
-      categories: biz['categories'].map{|category| category['title']}.join(', '),
-      image_url: biz['image_url'],
-      rating: biz['rating'],
-      list_id: 1
-      )
-    end
-    render json: Location.all.to_json
-
-  end
+  # end
 
   def index
     locations = Location.all
